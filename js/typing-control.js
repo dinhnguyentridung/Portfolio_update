8937fde076fcc22e5518921f194e1b93f8e6076d
dinhnguyentridung <145350030+dinhnguyentridung@users.noreply.github.com>
2023-10-02 01:17:19 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Designer.", "Copywriter."],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
